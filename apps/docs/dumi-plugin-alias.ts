ce8a5ac913cf49f592c0f38db82d21d1a69f91a3
brokun <brokun0128@gmail.com>
2023-10-03 01:11:06 +0800

    chore: ignore pnpm-lock file & remove unused script

--- apps/docs/dumi-plugin-alias.ts
+++ apps/docs/dumi-plugin-alias.ts
@@ -1,12 +1,6 @@
 import type { IApi } from 'dumi';
 import execa from 'execa';
 
-// dumi 2 不感知 monorepo 了，所以包名到 src 的 alias 要自己手动配置，比如 alias: { pkg: path.join(__dirname, 'packages/pkg/src') }
-// alias: {
-//   '@alipay/ai-infra-dynamic-layout': path.join(__dirname, 'packages/dynamic-layout/src'),
-//   '@alipay/ai-infra-layout-generator': path.join(__dirname, 'packages/layout-generator/src'),
-// },
-
 export default (api: IApi) => {
   api.modifyWebpackConfig(async (memo) => {
     const { stdout } = execa.commandSync('pnpm m ls --json --depth=-1');
