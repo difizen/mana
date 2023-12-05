e0ec321b6b38c6f5e06423f5f56bc9fcff992859
sunshinesmilelk <ximo.lk@antgroup.com>
2023-12-05 19:32:06 +0800

    fix: delete filetree menu

--- packages/mana-app/src/file-tree/index.ts
+++ packages/mana-app/src/file-tree/index.ts
@@ -2,6 +2,5 @@ export * from './files';
 export * from './file-tree';
 export * from './file-service';
 export * from './file-tree-model';
-export * from './file-tree-menu';
 export * from './file-tree-view';
 export * from './module';
