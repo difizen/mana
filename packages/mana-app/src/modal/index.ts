0fd55904ff8663598180c066bce8c7dcaeb07d89
brokun <yukun.wyk@antgroup.com>
2024-02-22 17:55:06 +0800

    feat(app): allow custom modal rendering logic

--- packages/mana-app/src/modal/index.ts
+++ packages/mana-app/src/modal/index.ts
@@ -1,14 +1,20 @@
 import { createViewPreference, ManaModule, PortalSlotId } from '@difizen/mana-core';
+
+import { ModalApplicationContribution } from './modal-contribution';
+import { ModalContribution } from './modal-protocol';
 import { ModalRenderView } from './modal-render';
-import { ModalContribution, ModalService } from './modal-service';
+import { ModalService } from './modal-service';
 
 export * from './modal-render';
 export * from './modal-service';
+export * from './modal-protocol';
+export * from './modal-contribution';
 
 export const ModalModule = ManaModule.create()
   .register(
     ModalService,
     ModalRenderView,
+    ModalApplicationContribution,
     createViewPreference({
       view: ModalRenderView,
       slot: PortalSlotId,
