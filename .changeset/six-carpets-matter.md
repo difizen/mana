2574a0abfa5414a5a520296203ed7a761a9870a5
sunshinesmilelk <1176136681@qq.com>
2024-05-07 11:36:30 +0800

    chore(ui): add father config

---
+++ .changeset/six-carpets-matter.md
@@ -0,0 +1,13 @@
+---
+'@difizen/mana-ui': patch
+'@difizen/mana-docs': patch
+'@difizen/mana-app': patch
+'@difizen/mana-common': patch
+'@difizen/mana-core': patch
+'@difizen/mana-l10n': patch
+'@difizen/mana-observable': patch
+'@difizen/mana-react': patch
+'@difizen/mana-syringe': patch
+---
+
+chore: add father config
--- packages/mana-ui/.fatherrc.ts
+++ packages/mana-ui/.fatherrc.ts
@@ -6,7 +6,14 @@ export default {
   cjs: {
     output: 'lib',
   },
-  extraBabelPlugins: [],
+  extraBabelPlugins: [
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-transform-flow-strip-types'],
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+    'babel-plugin-parameter-decorator',
+  ],
   extraBabelPresets: [
     [
       '@babel/preset-react',
