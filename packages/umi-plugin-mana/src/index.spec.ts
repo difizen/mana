7b41806eaeb7e7d67beef378a4a65bdefbae5e8f
brokun <yukun.wyk@antgroup.com>
2024-08-21 12:04:47 +0800

    test(umi-plugin): init unit test

--- packages/umi-plugin-mana/src/index.spec.ts
+++ packages/umi-plugin-mana/src/index.spec.ts
@@ -1,7 +1,26 @@
 import assert from 'assert';
 
+import { pageContent } from './mana-runtime';
+
 describe('umi-plugin-mana', () => {
-  it('#base', async () => {
-    assert(true);
+  it('#page content', async () => {
+    assert(
+      pageContent('my-slot') ===
+        `(async () => {
+const { Slot } = await import('@difizen/mana-app');
+const { Outlet } = await import('umi');
+
+const Page = ({ children, ...props }) => {
+  return (
+  <Slot name="my-slot" viewProps={props}>
+    <Outlet />
+  </Slot>
+  );
+};
+
+return Page
+})()
+`,
+    );
   });
 });
