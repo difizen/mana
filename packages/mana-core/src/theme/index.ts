c24e5974abd064d3af1d27f321745daa48183a74
brokun <yukun.wyk@antgroup.com>
2024-08-25 15:19:57 +0800

    fix(core): export color protocol

--- packages/mana-core/src/theme/index.ts
+++ packages/mana-core/src/theme/index.ts
@@ -14,6 +14,7 @@ import { ThemeService } from './theme-service';
 export * from './basic';
 export * from './color';
 export * from './theme-service';
+export * from './protocol';
 
 export const ThemeModule = ManaModule.create().register({
   token: ThemeService,
