c404d7532f0a8206af3fcfa4b1b8fc96c53034d7
brokun <yukun.wyk@antgroup.com>
2024-11-15 01:48:15 +0800

    test(syringe): side option

--- packages/mana-syringe/src/side-option.spec.ts
+++ packages/mana-syringe/src/side-option.spec.ts
@@ -1,4 +1,5 @@
 import assert from 'assert';
+
 import { register, GlobalContainer } from './container';
 import { singleton } from './decorator';
 import { registerSideOption } from './side-option';
@@ -15,5 +16,24 @@ describe('side option', () => {
     const foo = GlobalContainer.get(Foo);
     const sideValue = GlobalContainer.get<boolean>('side');
     assert(foo instanceof Foo && sideValue === true);
+  });
+
+  it('#class extend side option', () => {
+    const side = (name: string) => {
+      return (target: any) => {
+        Reflect.defineMetadata('name', name, target);
+      };
+    };
+    @side('Foo')
+    @singleton()
+    class Foo {}
+    register(Foo);
+
+    @side('FooBar')
+    @singleton()
+    class FooBar extends Foo {}
+    register(FooBar);
+
+    assert(Reflect.getMetadata('name', FooBar) === 'FooBar');
   });
 });
